fe170062d27be9af4448f608df2aba185a7f035b
Oleksandr <aleksandrpaskoo22@gmail.com>
2025-03-08 12:43:52 +0100

    Implement

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import os
 
 
 def copy_file(command: str) -> None:
-    result = command.split()
-    if len(result) == 3:
-        operation, file1_name, file2_name = result
+    parts = command.split()
+    if len(parts) == 3:
+        operation, file1_name, file2_name = parts
         if file1_name != file2_name and operation == "cp" and os.path.exists(file1_name):
             with open(file1_name, "r") as file_in, open(file2_name, "w") as file_out:
                 for line in file_in.readlines():
